f2c50865308d9a1826fed4633b10f8534ece40a6
fatehu <1605502591@qq.com>
2026-01-21 21:09:35 +0800

    从远程仓库移除误上传的文件夹

--- research-assistant/backend/init.sql
+++
@@ -1,5 +0,0 @@
--- 启用 pgvector 向量扩展
-CREATE EXTENSION IF NOT EXISTS vector;
-
--- 验证扩展已安装
-SELECT extname, extversion FROM pg_extension WHERE extname = 'vector';
--- research-assistant/frontend/src/pages/literature/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as LiteraturePage } from './LiteraturePage'
-export { default as PaperDetailPanel } from './PaperDetailPanel'
